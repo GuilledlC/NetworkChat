8c055cfa38b57f3e45fb7f2490accb6afdd66958
Guille <piratitag@gmail.com>
2020-10-28 18:39:51 +0100

    Add files via upload

---
+++ ClientApp/ClientApp/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MsgTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IPBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PortBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.NameButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(246, 432);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(472, 22);
+            this.MessageBox.TabIndex = 1;
+            this.MessageBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MessageBox_KeyDown);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(724, 432);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(102, 23);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.SystemColors.Window;
+            this.OutputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.OutputBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.OutputBox.Location = new System.Drawing.Point(247, 33);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(579, 393);
+            this.OutputBox.TabIndex = 3;
+            this.OutputBox.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(226, 194);
+            this.listBox1.TabIndex = 4;
+            // 
+            // MsgTimer
+            // 
+            this.MsgTimer.Interval = 10000;
+            this.MsgTimer.Tick += new System.EventHandler(this.MsgTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Users";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP Adress";
+            // 
+            // IPBox
+            // 
+            this.IPBox.Location = new System.Drawing.Point(15, 332);
+            this.IPBox.Name = "IPBox";
+            this.IPBox.Size = new System.Drawing.Size(226, 22);
+            this.IPBox.TabIndex = 8;
+            this.IPBox.Text = "fe80::711c:5c55:292f:c656%21";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Port";
+            // 
+            // PortBox
+            // 
+            this.PortBox.Location = new System.Drawing.Point(15, 375);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(226, 22);
+            this.PortBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Name";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(13, 252);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(228, 22);
+            this.NameBox.TabIndex = 12;
+            // 
+            // NameButton
+            // 
+            this.NameButton.Location = new System.Drawing.Point(13, 281);
+            this.NameButton.Name = "NameButton";
+            this.NameButton.Size = new System.Drawing.Size(227, 23);
+            this.NameButton.TabIndex = 14;
+            this.NameButton.Text = "Change Name";
+            this.NameButton.UseVisualStyleBackColor = true;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(14, 404);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(227, 23);
+            this.ConnectButton.TabIndex = 15;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(14, 433);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(226, 23);
+            this.DisconnectButton.TabIndex = 16;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(838, 469);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.NameButton);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PortBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.IPBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.MessageBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MessageBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer MsgTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IPBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PortBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button NameButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+    }
+}
+
---
+++ ClientApp/ClientApp/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    public partial class Form1 : Form
+    {
+        string name = "Guille"; //Guille
+        string address = "fe80::b1bb:d56c:b2d9:a5af%15"; //fe80::b1bb:d56c:b2d9:a5af%15
+        int port = 11111; //11111
+
+        public static void SendMessage(Socket sender, string message)
+        {
+            byte[] messageSent = Encoding.ASCII.GetBytes(message + "\b");
+            int byteSent = sender.Send(messageSent);
+        }
+
+        private void Show(string text)
+        {
+            OutputBox.AppendText(text);
+        }
+
+        private void Send(string message)
+        {
+            Font bold = new Font(OutputBox.Font, FontStyle.Bold);       //Self explanatory, the
+            Font regular = new Font(OutputBox.Font, FontStyle.Regular); //bold and regular fonts
+
+            string user = name + ": ";
+            int indent = TextRenderer.MeasureText(user, bold).Width - 6; //Indentation value
+
+            if (MsgTimer.Enabled) //If it's within the time it can be intended
+            {
+                OutputBox.SelectionIndent = indent; //Indentation
+                OutputBox.SelectionFont = regular;
+                OutputBox.AppendText(message + "\n");
+            }
+            else
+            {
+                OutputBox.SelectionIndent = 0; //No indentation
+                OutputBox.SelectionFont = bold;
+                OutputBox.AppendText(user);
+                OutputBox.SelectionFont = regular;
+                OutputBox.AppendText(message + "\n");
+            }
+            MessageBox.Text = ""; //Resets the message box
+            MsgTimer.Stop();
+            MsgTimer.Start(); //Starts the 5 second timer for indentation
+        }
+
+        private bool HasText(string text)
+        {
+            foreach (char c in text)
+            {
+                if (c > 32)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Show("Initialized\n");
+            //ExecuteClient(address, port);
+        }
+
+        private void MsgTimer_Tick(object sender, EventArgs e)
+        {
+            MsgTimer.Stop();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (HasText(MessageBox.Text))
+                Send(MessageBox.Text);
+            ExecuteClient(address, port);
+        }
+
+        private void MessageBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Enter) && HasText(MessageBox.Text))
+            {
+                Send(MessageBox.Text);
+                e.SuppressKeyPress = true; //Stops the DING sound when pressing Enter
+            }
+        }
+
+        public void ExecuteClient(string addr, int prt)
+        {
+            try
+            {
+                string address = addr;
+                int port = prt;
+
+                //Establish the remote endpoint for the socket.
+                IPHostEntry iPHost = Dns.GetHostEntry(Dns.GetHostName());
+                //IPAddress ipAddr = iPHost.AddressList[0]; //This is for local IP Address
+                IPAddress ipAddr = IPAddress.Parse(address);
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+                //Creation of the TCP/IP socket using Socket Class Constructor
+                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(localEndPoint); //Connect Socket to the remote endpoint using Connect() method
+                    SendMessage(sender, "RECEIVED");
+
+                    //Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString()); //We print the EndPoing info
+                    Show("Connected to " + addr + "!\n");
+
+                    while (true)
+                    {
+
+                        //Thread ReceiveThread = new Thread(() => SendMessage(sender));
+                        //ReceiveThread.Start();
+
+                        //ReceiveMessage(sender);
+                    }
+                    //Close Socket using the Close() method
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                //Manage the Socket's exceptions
+                catch (ArgumentNullException ane)
+                {
+                    Show("ArgumentNullException: " + ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Show("SocketException: " + se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Show("Unexpected exception: " + e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Show(e.ToString());
+            }
+            //Console.ReadLine();
+        }
+    }
+}
