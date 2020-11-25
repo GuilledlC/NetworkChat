ead875cfe149dccd5815b6dc2ec2f5bdca5c9f09
Guille <piratitag@gmail.com>
2020-11-25 18:36:00 +0100

    Update and rename MessageTypes.cs to FinalServer/MessageTypes.cs

---
+++ FinalServer/MessageTypes.cs
@@ -0,0 +1,11 @@
+namespace FinalServer
+{
+    enum MessageTypes
+    {
+        Error = -1,
+		Disconnected = 0,
+		Joined = 1,
+		Any = 2,
+		NameChange = 3,
+    }
+}
--- MessageTypes.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalServer
-{
-    enum MessageTypes
-    {
-        Disconnected = 0,
-        Joined = 1,
-        Any = 2,
-    }
-}
