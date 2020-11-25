f9b9b7345609d86881d147924ee061e5313d1d7b
Guille <piratitag@gmail.com>
2020-11-25 18:37:33 +0100

    Update Program.cs

--- FinalServer/FinalServer/Program.cs
+++ FinalServer/FinalServer/Program.cs
@@ -20,19 +20,6 @@ using System.Threading;
 
 namespace FinalServer
 {
-	enum MessageTypes
-	{
-		Error = -1,
-		Disconnected = 0,
-		Joined = 1,
-		Any = 2,
-		NameChange = 3,
-	}
-
-	//Types of message:
-	// 1: Joined the server
-	// 0: Left the server <- default message if none is specified, user is kicked lol
-	// 2: Any message
 	class Message
 	{
 		public string text;
