f8e45da98a9cb7658def98b8edcc938f98fb15ab
Guille <piratitag@gmail.com>
2021-02-05 03:44:07 +0100

    Update Program.cs
    Updated the SendMessage method to take an argument for the Message Type

--- ClientApp/ClientApp/Program.cs
+++ ClientApp/ClientApp/Program.cs
@@ -27,10 +27,15 @@ namespace ClientApp
 {
     public partial class Form1 : Form
     {
-        string name = "Guille"; //Guille
-        string address = "fe80::b1bb:d56c:b2d9:a5af%16"; //fe80::b1bb:d56c:b2d9:a5af%16
+        static bool connected = false;
+        static string name = "Guille"; //Guille
+        string address = "IP"; //IP
         int port = 11111; //11111
+        Socket server;
+        Thread ReceiveThread;
 
+        //https://docs.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls?view=netframeworkdesktop-4.8
+        
         private delegate void SafeCallDelegate(string text);
         private void WriteTextSafe(string text)
         {
@@ -47,7 +52,6 @@ namespace ClientApp
 
         private void Print(string text)
         {
-            //OutputBox.AppendText(text);
             WriteTextSafe(text);
         }
 
@@ -63,17 +67,23 @@ namespace ClientApp
             return false;
         }
 
-        public static void SendMessage(Socket sender, string message)
+        public static void SendMessage(Socket sender, string message, int type)
         {
-            byte[] messageSent = Encoding.ASCII.GetBytes(message + "\b");
-            int byteSent = sender.Send(messageSent);
+            if (connected)
+            {
+                byte[] messageSent = Encoding.ASCII.GetBytes(message + "\b" + name + "\b"+ type);
+                int byteSent = sender.Send(messageSent);
+            }
         }
 
         private void ReceiveMessage(Socket sender)
         {
-            byte[] buffer = new byte[2048];
-            int numByte = sender.Receive(buffer);
-            Print(Encoding.ASCII.GetString(buffer, 0, numByte));
+            while (true)
+            {
+                byte[] buffer = new byte[2048];
+                int numByte = sender.Receive(buffer);
+                Print("\n"+Encoding.ASCII.GetString(buffer, 0, numByte));
+            }
         }
 
         private void PrintMessage(string user, string message)
@@ -102,67 +112,83 @@ namespace ClientApp
             MsgTimer.Start(); //Starts the 5 second timer for indentation
         }
 
-        public void ExecuteClient(string addr, int prt)
+        public void Disconnect()
         {
             try
             {
-                string address = addr;
-                int port = prt;
+                //Close Socket using the Close() method
+                SendMessage(server, "DISCONNECT", 0);
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+                connected = false;
+                ReceiveThread.Abort();
+            }
+            catch(Exception e)
+            {
+                Print("\n(Un)expected exception:\n\n" + e.ToString() + "\n");
+            }
+        }
 
-                Print("Trying to connect to " + addr + ":" + port);
-
-                //Establish the remote endpoint for the socket.
-                IPHostEntry iPHost = Dns.GetHostEntry(Dns.GetHostName());
-                //IPAddress ipAddr = iPHost.AddressList[0]; //This is for local IP Address
-                IPAddress ipAddr = IPAddress.Parse(address);
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
-                //Creation of the TCP/IP socket using Socket Class Constructor
-                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
+        public void ExecuteClient(string addr, int prt)
+        {
+            if (!connected)
+            {
                 try
                 {
-                    sender.Connect(localEndPoint); //Connect Socket to the remote endpoint using Connect() method
-                    SendMessage(sender, "RECEIVED\bCLIENT\b2");
+                    string address = addr;
+                    int port = prt;
 
-                    //Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString()); //We print the EndPoing info
-                    Print("\nConnected to " + addr + "!\n");
+                    Print("\nTrying to connect to " + addr + ":" + port + "\n");
 
-                    Thread ReceiveThread = new Thread(() => ReceiveMessage  (sender));
-                    ReceiveThread.Start();
+                    //Establish the remote endpoint for the socket.
+                    IPHostEntry iPHost = Dns.GetHostEntry(Dns.GetHostName());
+                    //IPAddress ipAddr = iPHost.AddressList[0]; //This is for local IP Address
+                    IPAddress ipAddr = IPAddress.Parse(address);
+                    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+                    //Creation of the TCP/IP socket using Socket Class Constructor
+                    server = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                    while(false)
+                    try
                     {
-                        //Close Socket using the Close() method
-                        sender.Shutdown(SocketShutdown.Both);
-                        sender.Close();
+                        server.Connect(localEndPoint); //Connect Socket to the remote endpoint using Connect() method
+                        SendMessage(server, "RECEIVED"/*\bCLIENT\b2"*/, 2);
+
+                        //Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString()); //We print the EndPoing info
+                        Print("\nConnected to " + addr + ":" + port + "!\n");
+                        connected = true;
+
+                        ReceiveThread = new Thread(() => ReceiveMessage(server));
+                        ReceiveThread.Start();
                     }
-                }
-                //Manage the Socket's exceptions
-                catch (ArgumentNullException ane)
-                {
-                    Print("ArgumentNullException: " + ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Print("SocketException: " + se.ToString());
+                    //Manage the Socket's exceptions
+                    catch (ArgumentNullException ane)
+                    {
+                        Print("\nArgumentNullException:\n\n" + ane.ToString() + "\n");
+                    }
+                    catch (SocketException se)
+                    {
+                        Print("\nSocketException:\n\n" + se.ToString() + "\n");
+                    }
+                    catch (Exception e)
+                    {
+                        Print("\nUnexpected exception:\n\n" + e.ToString() + "\n");
+                    }
                 }
                 catch (Exception e)
                 {
-                    Print("Unexpected exception: " + e.ToString());
+                    Print(e.ToString());
                 }
+                //Console.ReadLine();
             }
-            catch (Exception e)
-            {
-                Print(e.ToString());
-            }
-            //Console.ReadLine();
         }
 
         public Form1()
         {
             InitializeComponent();
-            Print("Initialized\n");
-            //ExecuteClient(address, port);
+            NameBox.Text = name;
+            IPBox.Text = address;
+
+            PrintMessage("Client", "Initialized\n");
         }
 
         private void MsgTimer_Tick(object sender, EventArgs e)
@@ -170,26 +196,64 @@ namespace ClientApp
             MsgTimer.Stop();
         }
 
-        private void SendButton_Click(object sender, EventArgs e)
+        private void SendButton_Click(object sender, EventArgs e) //Send
         {
             if (HasText(MessageBox.Text))
-                //SendMessage(MessageBox.Text);
+                SendMessage(server, MessageBox.Text, 2);
                 MessageBox.Text = ""; //Resets the message box
         }
 
-        private void MessageBox_KeyDown(object sender, KeyEventArgs e)
+        private void MessageBox_KeyDown(object sender, KeyEventArgs e) //Send (Enter)
         {
             if ((e.KeyCode == Keys.Enter) && HasText(MessageBox.Text))
             {
-                //SendMessage(MessageBox.Text);
+                SendMessage(server, MessageBox.Text, 2);
                 MessageBox.Text = ""; //Resets the message box
                 e.SuppressKeyPress = true; //Stops the DING sound when pressing Enter
             }
         }
 
-        private void ConnectButton_Click(object sender, EventArgs e)
+        private void ConnectButton_Click(object sender, EventArgs e) //Connect
         {
-            ExecuteClient(address, port);
+            address = IPBox.Text; //Set the IP Address
+            try //Try to convert the Port
+            {
+                port = Convert.ToInt32(PortBox.Text);
+                ExecuteClient(address, port);
+            }
+            catch(Exception ex) //If it's not an integer it doesn't connect
+            {
+                Print("\n(Un)expected exception:\n\n" + ex.ToString() + "\n");
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e) //Disconnect
+        {
+            Disconnect();
+        }
+
+        private void NameButton_Click_1(object sender, EventArgs e) //Change name
+        {
+            SendMessage(server, name + " has changed their name to " + NameBox.Text, 3);
+            name = NameBox.Text;
+        }
+
+        private void NameBox_KeyDown_1(object sender, KeyEventArgs e) //Change name (Enter)
+        {
+            if ((e.KeyCode == Keys.Enter) && HasText(NameBox.Text))
+            {
+                SendMessage(server, name + " has changed their name to " + NameBox.Text, 3);
+                name = NameBox.Text;
+                e.SuppressKeyPress = true; //Stops the DING sound when pressing Enter
+            }
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e) //Auto Scroll
+        {
+            //Set the current caret position to the end
+            OutputBox.SelectionStart = OutputBox.Text.Length;
+            //Scroll it automatically
+            OutputBox.ScrollToCaret();
         }
     }
 }
